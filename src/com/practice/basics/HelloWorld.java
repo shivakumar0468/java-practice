2ec3a7fbca46d0a09e36bbfcf9a05665df25d194
shivakumar0468 <shivasavalapuram@gmail.com>
2023-08-16 12:36:39 +0530

    my first commit

---
+++ src/com/practice/basics/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.practice.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD .....");
+		System.out.println("hi");
+		System.out.println("name :");
+		System.out.println("shiva kumar");
+		System.out.println("ph number");
+		System.out.println("8978915521");
+		System.out.println("DOB");
+		System.out.println("21/05/2001");
+		System.out.println("gender :");
+		System.out.println("male");
+	}
+
+}
