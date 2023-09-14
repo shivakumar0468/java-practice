3d7d9dbe944bdec8220196c3b26e3826b1b78833
shivakumar0468 <shivasavalapuram@gmail.com>
2023-09-14 09:48:24 +0530

    modified

--- src/com/pratice/programs/patterns/DiamondPattern.java
+++ src/com/pratice/programs/patterns/DiamondPattern.java
@@ -8,7 +8,7 @@ public class DiamondPattern {
 		System.out.println("Enter the no of rows:");
 		int n = sc.nextInt();
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i <n; i++) {
 			for (int j = n; j > i; j--) {
 				System.out.print(" ");
 			}
@@ -18,7 +18,7 @@ public class DiamondPattern {
 			}
 			System.out.println();
 		}
-		for (int i = 1; i <= n - 1; i++) {
+		for (int i = 0; i <=n - 1; i++) {
 			for (int j = 0; j < i; j++) {
 				System.out.print(" ");
 
