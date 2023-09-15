df0f394c1a0a53ca915ba168021fcc6d901be451
shivakumar0468 <shivasavalapuram@gmail.com>
2023-09-15 09:34:55 +0530

    practice 4

---
+++ src/com/pratice/programs/patterns/Example1.java
@@ -0,0 +1,19 @@
+package com.pratice.programs.patterns;
+
+import java.util.Scanner;
+
+public class Example1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of rows: ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
---
+++ src/com/pratice/programs/patterns/Example4.java
@@ -0,0 +1,25 @@
+package com.pratice.programs.patterns;
+
+import java.util.Scanner;
+
+public class Example4 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no of rows: ");
+		int n=sc.nextInt();
+		
+		for(int i=n;i>0;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
