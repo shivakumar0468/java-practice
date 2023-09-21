d5ea3afe1c474ba59a9003218aefa7c49566bf8b
shivakumar0468 <shivasavalapuram@gmail.com>
2023-09-21 11:05:23 +0530

    modified

--- src/com/practice/strings/RemoveAllVowels.java
+++ src/com/practice/strings/RemoveAllVowels.java
@@ -1,26 +1,30 @@
 package com.practice.strings;
 
 import java.util.Scanner;
-
+/*
+ * 4. Write a program to remove all the vowels from a string.
+Input:
+Enter a string: Hello World
+Output:
+The string without vowels is: Hll Wrld
+ */
 public class RemoveAllVowels {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a String: ");
 		String str = sc.nextLine();
-		str=str.toLowerCase();
-		String s1="";
-		
+		str = str.toLowerCase();
+		String s1 = "";
 
-		for (int i = 0; i <str.length(); i++) {
-			char c=str.charAt(i);
-			if (c == 'a' || c== 'e' || c== 'i' || c== 'o'
-					|| c== 'u') {
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
 				continue;
 
-			}else {
-				
+			} else {
+
 			}
-			s1+=c;
+			s1 += c;
 		}
 		System.out.println(s1);
 		sc.close();
--- src/com/practice/strings/ValidPalindrome.java
+++ src/com/practice/strings/ValidPalindrome.java
@@ -1,7 +1,15 @@
 package com.practice.strings;
 
 import java.util.Scanner;
+/*
+ * 6.Write a program to check whether a string is a valid palindrome ignoring
+case sensitivity and white spaces.
+Input:
+Enter a string: A man a plan a canal Panama
+Output:
+The string is a valid palindrome.
 
+ */
 public class ValidPalindrome {
 	public static void main(String[] args) {
 
